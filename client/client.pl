802b23e4d578350530e30589db59a02fdb13730b
Mario Lang <mlang@tugraz.at>
2013-09-26 11:44:49 +0200

    Add client options.

--- client/client.pl
+++ client/client.pl
@@ -1,34 +1,37 @@
 #!/usr/bin/perl
 
 use strict;
+use Getopt::Std;
 use Net::MessageBus;
 use Data::Dumper;
 
+my %opt;
+getopts ('bd:o:', \%opt);
 
-    my $MessageBus = Net::MessageBus->new(server => '129.27.9.27',
-					  port=>15000,
-                          group => 'backend',
-                          sender => 'simon',
-    username=>'john',
-    password=>'1234',
-    );
-
+my $MessageBus = Net::MessageBus->new(
+  server => '129.27.9.27',
+  port=>15000,
+  group => 'backend',
+  sender => 'simon',
+  username=>'john',
+  password=>'1234',
+);
 
 $MessageBus->subscribe_all;
 
+while (my $message = $MessageBus->next_message()) {
+  if ($opt{b}) { print "\007"; }
+  print $message->sender()." sent ".$message->type().":\n";
+  print $message->payload()->{'message'};
+  chomp($message->payload()->{'message'});
+  if ($opt{o}) {
+    open(MYFILE, '>'.$opt{o});
+  } else {
+    $opt{d} = "" unless $opt{d};
+    open(MYFILE, '>'.$opt{d}.'/'.$message->sender());
+  }
 
- while (my $message = $MessageBus->next_message()) {
-     print "Sender:".$message->sender()."\n";
-        print $message->type()."\n";
+  print  MYFILE $message->payload()->{'message'};
+  close (MYFILE);
+}
 
-     open(MYFILE, '>'.$message->sender());
-
-	my $pl=$message->payload();
-	
-     print $pl->{'message'};
-	print  MYFILE $pl->{'message'};
-     close (MYFILE);
-
-    }
-
-#$MessageBus->subscribe_to_all();
